bee8e1cf0420222588cc58611dd8ae1cafca717a
Alim Mughanil Karim <alim.mughanil23@gmail.com>
2022-02-04 08:34:44 +0700

    change meta content of fb OG

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,8 +7,8 @@
     <meta name="facebook-domain-verification" content="j7gvwt2pwgmbzw9tbfvo40u17haw59" />
     <meta property='fb:app_id' content='1092848738224575'/>   
     <meta property="og:title" content="Hobi Sedekah">
-    <meta property="og:url" content="https://hobisedekah.com">
-    <meta property="og:description" content="Hidup Berkah Berlimpah">
+    <meta property="og:url" content="https://hobisedekah.id">
+    <meta property="og:description" content="#HidupBerkahBerlimpah">
     <meta property="og:type" content="website">
     <meta property="og:image" content="https://hobisedekah.com/img/logo.png"> 
     
